2af7ce0740067615bab6215c2634ce9a54724f2a
sramelot <sylvain.ramelot@student.uclouvain.be>
2015-05-08 00:25:09 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/src/main/java/com/lsinf1225/groupe_t/bartender/model/Order.java

---
+++ app/src/main/java/com/lsinf1225/groupe_t/bartender/activity/adapter/MyOrderDetailsListAdapter.java
@@ -0,0 +1,99 @@
+package com.lsinf1225.groupe_t.bartender.activity.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.BaseAdapter;
+
+import com.lsinf1225.groupe_t.bartender.R;
+import com.lsinf1225.groupe_t.bartender.model.Opinion;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sylvain on 08/05/2015.
+ */
+public class MyOrderDetailsListAdapter extends BaseAdapter{
+    /**
+     * Permet d'instancier un fichier xml de layout dans une vue.
+     */
+    private final LayoutInflater mInflater;
+
+    /**
+     * Liste des éléments de collection à mettre dans la liste.
+     */
+    private ArrayList<OrderDetails> collectedItems;
+
+    /**
+     * Constructeur.
+     *
+     * @param context        Contexte de l'application.
+     * @param collectedItems Liste des éléments de collection à placer dans la liste.
+     */
+    public MyOrderDetailsListAdapter(Context context, ArrayList<OrderDetails> collectedItems) {
+        mInflater = LayoutInflater.from(context);
+        this.collectedItems = collectedItems;
+    }
+
+    @Override
+    public int getCount() {
+        return collectedItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return collectedItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return collectedItems.get(position).getId_order();
+    }
+
+    /**
+     * Remplit chaque ligne de la liste avec un layout particulier.
+     *
+     * Cette méthode est appelée par Android pour construire la vue de la liste (lors de la
+     * construction de listview).
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Si la vue n'a pas encore été créé (typiquement lors du première affichage de la liste).
+        // Android recycle en effet les layout déjà chargés des éléments de la liste (par exemple
+        // lors du changement de l'ordre dans la liste.)
+
+        if (convertView == null) {
+            // Création d'un nouvelle vue avec le layout correspondant au fichier xml
+            convertView = mInflater.inflate(R.layout.collected_order_details_layout, parent, false);
+        }
+
+        // Récupération des deux éléments de notre vue dans le but d'y placer les données.
+        TextView drink = (TextView) convertView.findViewById(R.id.textview_show_comments_comment);
+        TextView quantity = (RatingBar) convertView.findViewById(R.id.show_comment_rating_bar);
+
+        // Récupération et placement des données.
+        OrderDetails collectedItem = collectedItems.get(position);
+        drink.setText(collectedItem.getDrink.getName_drink());
+        quantity.setText(Integer.toString(collectedItem.getQuantity()));
+
+        return convertView;
+    }
+
+    /**
+     * Change la liste des éléments de collection affichée.
+     *
+     * Permet de changer complètement la liste des éléments affichés dans la liste.
+     *
+     * @param newCollectedItems La nouvelle liste des éléments de collection à afficher.
+     *
+     * @post Les éléments de la liste ont été remplacés par les éléments passés en argument.
+     */
+    public void setCollectedItems(ArrayList<OrderDetails> newCollectedItems) {
+        this.collectedItems = newCollectedItems;
+        notifyDataSetChanged();
+    }
+}
+
