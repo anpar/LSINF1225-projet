e08e507bb679ee936763a222775efb91835dedef
GillonB <bastien.gillon@student.uclouvain.be>
2015-05-07 23:09:57 +0200

    bill show added

---
+++ app/src/main/java/com/lsinf1225/groupe_t/bartender/activity/ShowBillDetialsActivity.java
@@ -0,0 +1,9 @@
+package com.lsinf1225.groupe_t.bartender.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by gillonb on 07/05/15.
+ */
+public class ShowBillDetialsActivity extends Activity {
+}
