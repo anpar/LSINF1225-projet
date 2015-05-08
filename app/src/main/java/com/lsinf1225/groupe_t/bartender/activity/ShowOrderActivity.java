9def53045481faa38ac4bfbf8a660385270e6bef
anpar <paris.antoine.paris@gmail.com>
2015-05-08 12:15:04 +0200

    Fix buttons

--- app/src/main/java/com/lsinf1225/groupe_t/bartender/activity/ShowOrderActivity.java
+++ app/src/main/java/com/lsinf1225/groupe_t/bartender/activity/ShowOrderActivity.java
@@ -46,9 +46,14 @@ public class ShowOrderActivity extends Activity implements AdapterView.OnItemCli
 
         int table_number = getIntent().getIntExtra("table_number", -1);
         Button closeBill = (Button) findViewById(R.id.button_close_bill);
+        Button newOrder = (Button) findViewById(R.id.new_bill_table_number);
+        EditText newOrderTable = (EditText) findViewById(R.id.show_order_table_number);
 
         if(table_number == -1) {
             closeBill.setVisibility(Button.INVISIBLE);
+        } else {
+            newOrder.setVisibility(Button.INVISIBLE);
+            newOrderTable.setVisibility(Button.INVISIBLE);
         }
 
         // Création de l'adapter pour faire la liaison entre les données (collectedItems) et
@@ -93,7 +98,7 @@ public class ShowOrderActivity extends Activity implements AdapterView.OnItemCli
         // "Aucun élément n'est présent dans votre collection).
         if (collectedItems.isEmpty()) {
             if (table_number == -1) {
-                BarTenderApp.notifyShort(R.string.show_list_error_no_item);
+                BarTenderApp.notifyShort(R.string.show_list_no_order);
             }
         }
     }
