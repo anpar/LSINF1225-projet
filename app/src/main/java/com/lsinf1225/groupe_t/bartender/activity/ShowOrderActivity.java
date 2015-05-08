171c949db12f559d82495cf607ba10490950efd8
anpar <paris.antoine.paris@gmail.com>
2015-05-08 15:48:35 +0200

    Disable go back in showOrder

--- app/src/main/java/com/lsinf1225/groupe_t/bartender/activity/ShowOrderActivity.java
+++ app/src/main/java/com/lsinf1225/groupe_t/bartender/activity/ShowOrderActivity.java
@@ -224,4 +224,21 @@ public class ShowOrderActivity extends Activity implements AdapterView.OnItemCli
             BarTenderApp.notifyShort(R.string.sorry_error);
         }
     }
+
+    /**
+     * Désactive le bouton de retour. Désactive le retour à l'activité précédente (donc l'écran de
+     * connexion dans ce cas-ci) et affiche un message indiquant qu'il faut se déconnecter.
+     */
+    @Override
+    public void onBackPressed() {
+        int table_number = getIntent().getIntExtra("table_number", -1);
+        Intent intent = new Intent(this, ShowBillActivity.class);
+        // On désactive le retour (car on se trouve au menu principal) en ne faisant
+        // rien dans cette méthode si ce n'est afficher un message à l'utilisateur.
+        if(table_number != -1) {
+            BarTenderApp.notifyShort(R.string.finish_order_to_go_back);
+        } else {
+            finish();
+        }
+    }
 }
