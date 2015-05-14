d21a441a0b74872c839599074f3036a3511d751c
anpar <paris.antoine.paris@gmail.com>
2015-05-14 17:42:28 +0200

    Fix average note refresh

--- app/src/main/java/com/lsinf1225/groupe_t/bartender/activity/ShowCommentsActivity.java
+++ app/src/main/java/com/lsinf1225/groupe_t/bartender/activity/ShowCommentsActivity.java
@@ -92,9 +92,7 @@ public class ShowCommentsActivity extends Activity {
         super.onResume();
         // La liste des éléments est ici rechargées car en cas de modification d'un élément, l'ordre
         // a peut-être changé.
-
         loadCollectedItems();
-
         myListViewAdapter.setCollectedItems(collectedItems);
     }
 }
