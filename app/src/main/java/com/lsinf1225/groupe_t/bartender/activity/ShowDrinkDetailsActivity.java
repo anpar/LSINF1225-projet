b1604aac33f5ce6a81db6f31834317bac1bf5a67
anpar <paris.antoine.paris@gmail.com>
2015-05-14 17:52:48 +0200

    Fix note refresh

--- app/src/main/java/com/lsinf1225/groupe_t/bartender/activity/ShowDrinkDetailsActivity.java
+++ app/src/main/java/com/lsinf1225/groupe_t/bartender/activity/ShowDrinkDetailsActivity.java
@@ -130,4 +130,13 @@ public class ShowDrinkDetailsActivity extends Activity {
             }
         }
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // La liste des éléments est ici rechargées car en cas de modification d'un élément, l'ordre
+        // a peut-être changé.
+        Drink.getDrinks();
+        currentDrink = Drink.get(currentDrink.getId_drink());
+    }
 }
